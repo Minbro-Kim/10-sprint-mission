02aef21ea52d7079f6ea649f0f55a80e197d76c3
Minbro-Kim <sojangjangi@gmail.com>
2026-03-06 19:21:52 +0900

    fix: profile join을 left join으로 변경

--- src/main/java/com/sprint/mission/discodeit/repository/UserRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/UserRepository.java
@@ -14,8 +14,8 @@ public interface UserRepository extends JpaRepository<User, UUID> {
 
   boolean existsByUsername(String username);
 
-  @Query("select u from User u join fetch u.userStatus join fetch u.profile where u.username = :username and u.password =:password")
-    // 유저상태와 프로필 정보를 유저 디티오에 같이 보내기 때문에 쿼리 3번 발생.패치조인으로 한번에 가져오기
+  @Query("select u from User u join fetch u.userStatus left join fetch u.profile where u.username = :username and u.password =:password")
+    // 유저상태와 프로필 정보를 유저 디티오에 같이 보내기 때문에 쿼리 3번 발생.패치조인으로 한번에 가져오기, 프로필 사진 없는 경우도 있으니까 레프트 조인
   Optional<User> findByUsernameAndPassword(String username, String password);
 
 }
