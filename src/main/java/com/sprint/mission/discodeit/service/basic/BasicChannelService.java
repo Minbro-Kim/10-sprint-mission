38749cee88bbff01151e139b0c3b07f387555374
Minbro-Kim <sojangjangi@gmail.com>
2026-03-07 19:08:41 +0900

    fix: 채널 생성시 채널 멤버의 ReadStatus의 lastReadAt을 Instant.Epoch으로 생성

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicChannelService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicChannelService.java
@@ -46,8 +46,9 @@ public class BasicChannelService implements ChannelService {
     //System.out.println("채널 저장 완료");
     //모든 사용자가 멤버!
     userRepository.findAll()//한번에 저장하는방법?
-        .forEach(m -> readStatusRepository.save(ReadStatus.create(m, channel, Instant.now())));
+        .forEach(m -> readStatusRepository.save(ReadStatus.create(m, channel, Instant.EPOCH)));
     //System.out.println("읽음 상태 저장 완료");
+    System.out.println("채널 생성 읽음 상태 초기값: " + Instant.EPOCH);
     return channelMapper.toDto(channel);
   }
 
@@ -61,7 +62,7 @@ public class BasicChannelService implements ChannelService {
     }
     members
         .forEach(m -> {
-          readStatusRepository.save(ReadStatus.create(m, channel, Instant.now()));
+          readStatusRepository.save(ReadStatus.create(m, channel, Instant.EPOCH));
         });
     return channelMapper.toDto(channel);
   }
