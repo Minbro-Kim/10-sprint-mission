56ff3b4994c522eb5028e1cf0d71c789a2b17691
Minbro-Kim <sojangjangi@gmail.com>
2026-03-09 17:02:00 +0900

    fix: user 프로필 업데이트시, 기존 프로필 없는 경우 npe 방지

--- src/main/java/com/sprint/mission/discodeit/entity/User.java
+++ src/main/java/com/sprint/mission/discodeit/entity/User.java
@@ -79,7 +79,8 @@ public class User extends BaseUpdatableEntity {
     if (newPassword != null && !newPassword.isEmpty() && !newPassword.equals(this.password)) {
       this.password = newPassword;
     }
-    if (newProfile != null && !newProfile.getId().equals(this.profile.getId())) {
+    if (newProfile != null && (this.profile == null || !newProfile.getId()
+        .equals(this.profile.getId()))) {
       this.profile = newProfile;
     }
 
