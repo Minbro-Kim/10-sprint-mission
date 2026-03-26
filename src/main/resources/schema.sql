9936fb3c6b8b7c931a1ed7788d99595d48e6a12c
Minbro-Kim <sojangjangi@gmail.com>
2026-03-26 10:45:38 +0900

    refactor: H2와의 호환성을 고려하여 Timestamp with time zone으로 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,11 +1,11 @@
 CREATE TABLE binary_contents
 (
     id           UUID PRIMARY KEY,
-    file_name    VARCHAR(255) NOT NULL,
-    size         BIGINT       NOT NULL,
-    bytes        BYTEA        NOT NULL,
-    content_type VARCHAR(100) NOT NULL,
-    created_at   TIMESTAMPTZ  NOT NULL
+    file_name    VARCHAR(255)             NOT NULL,
+    size         BIGINT                   NOT NULL,
+    bytes        BYTEA                    NOT NULL,
+    content_type VARCHAR(100)             NOT NULL,
+    created_at   TIMESTAMP WITH TIME ZONE NOT NULL
 );
 
 /*
@@ -17,12 +17,12 @@ ALTER TABLE binary_contents
 CREATE TABLE users
 (
     id         UUID PRIMARY KEY,
-    username   VARCHAR(50)  NOT NULL UNIQUE,
-    email      VARCHAR(100) NOT NULL UNIQUE,
-    password   VARCHAR(60)  NOT NULL,
+    username   VARCHAR(50)              NOT NULL UNIQUE,
+    email      VARCHAR(100)             NOT NULL UNIQUE,
+    password   VARCHAR(60)              NOT NULL,
     profile_id UUID UNIQUE,
-    created_at TIMESTAMPTZ  NOT NULL,
-    updated_at TIMESTAMPTZ,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE,
     CONSTRAINT fk_profile_id
         FOREIGN KEY (profile_id)
             REFERENCES binary_contents (id)
@@ -34,10 +34,10 @@ CREATE TABLE users
 CREATE TABLE user_statuses
 (
     id             UUID PRIMARY KEY,
-    user_id        UUID UNIQUE NOT NULL REFERENCES users (id) ON DELETE CASCADE,
-    last_active_at TIMESTAMPTZ NOT NULL,
-    created_at     TIMESTAMPTZ NOT NULL,
-    updated_at     TIMESTAMPTZ
+    user_id        UUID UNIQUE              NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+    last_active_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    created_at     TIMESTAMP WITH TIME ZONE NOT NULL,
+    updated_at     TIMESTAMP WITH TIME ZONE
 );
 
 CREATE TABLE channels
@@ -45,19 +45,19 @@ CREATE TABLE channels
     id          UUID PRIMARY KEY,
     name        VARCHAR(100),
     description VARCHAR(500),
-    type        VARCHAR(10) NOT NULL CHECK (type IN ('PUBLIC', 'PRIVATE')),
-    created_at  TIMESTAMPTZ NOT NULL,
-    updated_at  TIMESTAMPTZ
+    type        VARCHAR(10)              NOT NULL CHECK (type IN ('PUBLIC', 'PRIVATE')),
+    created_at  TIMESTAMP WITH TIME ZONE NOT NULL,
+    updated_at  TIMESTAMP WITH TIME ZONE
 );
 
 CREATE TABLE read_statuses
 (
     id           UUID PRIMARY KEY,
-    user_id      UUID        NOT NULL REFERENCES users (id) ON DELETE CASCADE,
-    channel_id   UUID        NOT NULL REFERENCES channels (id) ON DELETE CASCADE,
-    last_read_at TIMESTAMPTZ NOT NULL,
-    created_at   TIMESTAMPTZ NOT NULL,
-    updated_at   TIMESTAMPTZ,
+    user_id      UUID                     NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+    channel_id   UUID                     NOT NULL REFERENCES channels (id) ON DELETE CASCADE,
+    last_read_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    created_at   TIMESTAMP WITH TIME ZONE NOT NULL,
+    updated_at   TIMESTAMP WITH TIME ZONE,
     CONSTRAINT user_channel_id UNIQUE (user_id, channel_id)
 );
 
@@ -65,10 +65,10 @@ CREATE TABLE messages
 (
     id         UUID PRIMARY KEY,
     content    TEXT,
-    author_id  UUID        REFERENCES users (id) ON DELETE SET NULL,
-    channel_id UUID        NOT NULL REFERENCES channels (id) ON DELETE CASCADE,
-    created_at TIMESTAMPTZ NOT NULL,
-    updated_at TIMESTAMPTZ
+    author_id  UUID                     REFERENCES users (id) ON DELETE SET NULL,
+    channel_id UUID                     NOT NULL REFERENCES channels (id) ON DELETE CASCADE,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE
 );
 
 CREATE TABLE message_attachments
