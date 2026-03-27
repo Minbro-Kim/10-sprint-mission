28699e20e73e7317b19ccf4a129bbc93b23d5f59
Minbro-Kim <sojangjangi@gmail.com>
2026-03-27 16:20:45 +0900

    fix: 메세지 수정 공백 허용, null 불가

--- src/main/java/com/sprint/mission/discodeit/dto/message/MessageUpdateRequest.java
+++ src/main/java/com/sprint/mission/discodeit/dto/message/MessageUpdateRequest.java
@@ -1,7 +1,7 @@
 package com.sprint.mission.discodeit.dto.message;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 @Schema(description = "수정할 Message 내용")
 public record MessageUpdateRequest(
