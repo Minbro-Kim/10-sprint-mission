49e9eeef5f2bbc74f2819e3e3f5871e68895ee79
Minbro-Kim <sojangjangi@gmail.com>
2026-04-08 09:56:21 +0900

    feat: S3BinaryContentStorageTest 실패 케이스 추가

--- src/test/java/com/sprint/mission/discodeit/storage/S3BinaryContentStorageTest.java
+++ src/test/java/com/sprint/mission/discodeit/storage/S3BinaryContentStorageTest.java
@@ -9,6 +9,9 @@ import static org.mockito.Mockito.mock;
 import com.sprint.mission.discodeit.config.S3Properties;
 import com.sprint.mission.discodeit.dto.binarycontent.BinaryContentDto;
 import com.sprint.mission.discodeit.entity.Message;
+import com.sprint.mission.discodeit.exception.binarycontent.StorageDownloadFailedException;
+import com.sprint.mission.discodeit.exception.binarycontent.StorageGetFailedException;
+import com.sprint.mission.discodeit.exception.binarycontent.StorageUploadFailedException;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -24,12 +27,14 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import software.amazon.awssdk.awscore.exception.AwsErrorDetails;
 import software.amazon.awssdk.core.ResponseBytes;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.GetObjectResponse;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.S3Exception;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
 import software.amazon.awssdk.services.s3.presigner.model.PresignedGetObjectRequest;
@@ -68,6 +73,25 @@ class S3BinaryContentStorageTest {
 
   }
 
+  @Test
+  @DisplayName("실패: S3에 파일 업로드 중 오류 발생")
+  void putFileToS3Failure() {
+    //given
+    UUID fileId = UUID.randomUUID();
+    AwsErrorDetails errorDetails = AwsErrorDetails.builder()
+        .errorCode("S3_UPLOAD_ERROR")
+        .errorMessage("S3 서비스 응답 오류")
+        .build();
+    given(s3Properties.getBucket()).willReturn("bucketName");
+    given(s3Client.putObject(any(PutObjectRequest.class), any(RequestBody.class))).willThrow(
+        S3Exception.builder().awsErrorDetails(errorDetails).build());
+
+    //when&then
+    assertThrows(
+        StorageUploadFailedException.class,
+        () -> s3BinaryContentStorage.put(fileId, new byte[]{0, 12, 3}));
+
+  }
 
   @Test
   @DisplayName("성공: S3에서 바이트 코드 가져오기 성공")
@@ -96,6 +120,26 @@ class S3BinaryContentStorageTest {
   }
 
   @Test
+  @DisplayName("실패: S3에서 바이트 코드 가져오는 중 오류 발생")
+  void getFileToS3Failure() {
+    //given
+    UUID fileId = UUID.randomUUID();
+    AwsErrorDetails errorDetails = AwsErrorDetails.builder()
+        .errorCode("S3_GET_ERROR")
+        .errorMessage("S3 서비스 응답 오류")
+        .build();
+    given(s3Properties.getBucket()).willReturn("bucketName");
+    given(s3Client.getObjectAsBytes(any(GetObjectRequest.class))).willThrow(
+        S3Exception.builder().awsErrorDetails(errorDetails).build());
+
+    //when&then
+    assertThrows(
+        StorageGetFailedException.class,
+        () -> s3BinaryContentStorage.get(fileId));
+
+  }
+
+  @Test
   @DisplayName("성공: S3에서 파일 URL 받아서 리다이렉션 반환 성공")
   void downloadFileFromS3Success() throws Exception {
     //given
@@ -115,5 +159,29 @@ class S3BinaryContentStorageTest {
     //then
     assertEquals(HttpStatus.FOUND, result.getStatusCode());
     assertEquals(fakeUrl.toString(), result.getHeaders().getLocation().toString());
+  }
+
+  @Test
+  @DisplayName("실패: S3에서 파일 URL 받아서 리다이렉션 반환 중 오류 발생")
+  void downloadFileToS3Failure() {
+    //given
+    BinaryContentDto dto = new BinaryContentDto(UUID.randomUUID(), "fileName", 50L, "jpg");
+    PresignedGetObjectRequest presignedRequest = mock(PresignedGetObjectRequest.class);
+
+    AwsErrorDetails errorDetails = AwsErrorDetails.builder()
+        .errorCode("S3_GET_ERROR")
+        .errorMessage("S3 서비스 응답 오류")
+        .build();
+
+    given(s3Properties.getBucket()).willReturn("bucketName");
+    given(s3Properties.getPresignedUrlExpiration()).willReturn(600);
+    given(s3Presigner.presignGetObject(any(GetObjectPresignRequest.class))).willThrow(
+        S3Exception.builder().awsErrorDetails(errorDetails).build());
+
+    //when&then
+    assertThrows(
+        StorageDownloadFailedException.class,
+        () -> s3BinaryContentStorage.download(dto));
+
   }
 }
