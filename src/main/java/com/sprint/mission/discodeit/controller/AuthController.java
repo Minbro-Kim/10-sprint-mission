43bc7eb1b4aadb29ca395b5f0355f8104e4faeba
Minbro-Kim <sojangjangi@gmail.com>
2026-05-18 09:19:37 +0900

    feat: 사용자 권한 변경 권한을 관리자에게만 부여

--- src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.*;
@@ -38,6 +39,7 @@ public class AuthController {
     return ResponseEntity.ok(dto);
   }
 
+  @PreAuthorize("hasRole('ADMIN')")
   @PutMapping("/role")
   public ResponseEntity<UserDto> updateUserRole(
       @Valid @RequestBody UserRoleUpdateRequest userRoleUpdateRequest) {
