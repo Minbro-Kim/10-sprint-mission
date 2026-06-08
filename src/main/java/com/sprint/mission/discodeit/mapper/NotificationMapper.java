c548d96948813eea8f164999b6c0f4db6ddde4c3
Minbro-Kim <sojangjangi@gmail.com>
2026-06-08 18:36:34 +0900

    fix: NotificationMapper receiver id 매핑 수정

--- src/main/java/com/sprint/mission/discodeit/mapper/NotificationMapper.java
+++ src/main/java/com/sprint/mission/discodeit/mapper/NotificationMapper.java
@@ -3,9 +3,11 @@ package com.sprint.mission.discodeit.mapper;
 import com.sprint.mission.discodeit.dto.notification.NotificationDto;
 import com.sprint.mission.discodeit.entity.Notification;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface NotificationMapper {
 
+  @Mapping(source = "receiver.id", target = "receiverId")
   NotificationDto toDto(Notification notification);
 }
