e8b7f581de0b8572383d6f142c1cf300dc6034cc
Minbro-Kim <sojangjangi@gmail.com>
2026-06-17 19:06:08 +0900

    refactor: userupdated에서 개인 채널정보도 sse를 통해 변경되도록 수정

--- src/main/java/com/sprint/mission/discodeit/util/listener/UserRequiredEventListener.java
+++ src/main/java/com/sprint/mission/discodeit/util/listener/UserRequiredEventListener.java
@@ -1,9 +1,16 @@
 package com.sprint.mission.discodeit.util.listener;
 
+import com.sprint.mission.discodeit.dto.channel.ChannelDto;
+import com.sprint.mission.discodeit.dto.user.UserDto;
+import com.sprint.mission.discodeit.entity.ChannelType;
 import com.sprint.mission.discodeit.event.UserCreatedEvent;
 import com.sprint.mission.discodeit.event.UserDeletedEvent;
 import com.sprint.mission.discodeit.event.UserUpdatedEvent;
+import com.sprint.mission.discodeit.repository.ReadStatusRepository;
+import com.sprint.mission.discodeit.service.ChannelService;
 import com.sprint.mission.discodeit.service.SseService;
+import java.util.List;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
@@ -17,6 +24,7 @@ import org.springframework.transaction.event.TransactionalEventListener;
 public class UserRequiredEventListener {
 
   private final SseService sseService;
+  private final ChannelService channelService;
 
   @Async
   @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
@@ -29,6 +37,15 @@ public class UserRequiredEventListener {
       fallbackExecution = true)
   public void on(UserUpdatedEvent event) {
     sseService.broadcast("users.updated", event.userDto());
+    List<ChannelDto> myAllChannels = channelService.findAllByUserId(event.userDto().id());
+    myAllChannels.stream()
+        .filter(dto -> dto.type() == ChannelType.PRIVATE)
+        .forEach(dto -> {
+          List<UUID> memberIds = dto.participants().stream()
+              .map(UserDto::id)
+              .toList();
+          sseService.send(memberIds, "channels.updated", dto);
+        });
   }
 
   @Async
